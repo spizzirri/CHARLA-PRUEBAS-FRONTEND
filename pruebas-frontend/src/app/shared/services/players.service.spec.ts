7c9c720b7cc6633f17f00aea63e80f0db9280329
Damian Spizzirri <damianspizzirri@gmail.com>
2021-06-15 00:28:25 -0300

    agrega pruebas unitarias en players service

--- pruebas-frontend/src/app/shared/services/players.service.spec.ts
+++ pruebas-frontend/src/app/shared/services/players.service.spec.ts
@@ -1,16 +1,31 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-
+import { Observable } from 'rxjs';
 import { PlayersService } from './players.service';
 
 describe('PlayersService', () => {
   let service: PlayersService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(PlayersService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return an Observable"', ()=>{
+
+    const obs = service.getPlayer$();
+    expect(obs).toBeInstanceOf(Observable)
+  })
+
+  it('should set next value when the "setPlayer" method is invoked', (done)=>{
+    const obs = service.getPlayer$();
+    obs.subscribe( e => { expect(e).toEqual({Byear: '', ELO: '', federation: '', name: ''}); done()})
+    service.setPlayer({Byear: '', ELO: '', federation: '', name: ''})
+  })
 });
